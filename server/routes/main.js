f6627261a82781a0175dc27a50830eaf9267066d
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-11-21 20:28:44 +0530

    style, script

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,14 @@ const router = express.Router();
 
 //routes
 router.get('', (req, res) => {
-    res.render('index')
+    const locals = {
+        title : "Blog Website",
+        description : "Blog website made using node js, express and mongodb"
+    }
+    res.render('index', {locals})
+});
+router.get('/about', (req, res) => {
+    res.render('about')
 });
 
 module.exports = router;
