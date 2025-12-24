5010e6d5eff4e0b7c354ff4d46d98a07d955a9e7
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-12-25 03:59:54 +0530

    feat: enhance search functionality with search bar and results display

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const allButtons = document.querySelectorAll('.searchBtn');
+    const searchBar = document.querySelectorAll('.searchBar'); //alt shift down
+    const searchInput = document.getElementById('searchInput'); 
+    const searchClose = document.getElementById('searchClose');
+    
+    for(var i = 0; i<allButtons)
 
+})
