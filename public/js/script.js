2eb4e693355e48c885fdefa43f50d56a16f34a39
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-12-26 21:11:45 +0530

    fix: correct search bar selection and improve event listener setup

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
     const allButtons = document.querySelectorAll('.searchBtn');
-    const searchBar = document.querySelectorAll('.searchBar'); //alt shift down
+    const searchBar = document.querySelector('.searchBar'); //alt shift down
     const searchInput = document.getElementById('searchInput'); 
     const searchClose = document.getElementById('searchClose');
     
-    for(var i = 0; i<allButtons)
+    for(var i = 0; i<allButtons.length; i++){
+
+        allButtons[i].addEventListener('click', function(){
+            searchBar.style.visibility = 'visible';
+            searchBar.classList.add('open');
+            this.setAttribute('aria-expanded', 'true');
+            searchInput.focus();
+        });
+    }
+
+    searchClose.addEventListener('click', function(){
+            searchBar.style.visibility = 'hidden';
+            searchBar.classList.remove('open');
+            this.setAttribute('aria-expanded', 'false');
+        });
 
 })
