27081f957f58bc65c32c500f25fef769cd54abed
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-12-31 21:29:04 +0530

    fix: add missing session middleware configuration and correct syntax errors

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
+
 
 //db
-const connectDB = require('./server/config/db')
+const connectDB = require('./server/config/db');
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
@@ -15,6 +17,16 @@ connectDB();
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(session(){
+    secret: 'keyboard cat',
+    resave: false,
+    saveUnitialized: true,
+    store: MongoStore.create({
+        mongoURI: process.env.MONGODB_URI
+    }),
+})
 
 app.use(express.static('public'));
 
