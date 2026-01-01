70227bfb4aa809a608730d032bb8f4afbebce686
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2026-01-02 01:12:50 +0530

    feat: implement authentication middleware and add dashboard view

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,6 +6,30 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const adminLayout = '../views/layouts/admin';
+const jwtSecret = process.env.JWT_SECRET;  
+
+
+/*
+Middleware function to
+Check login
+*/
+const authMiddleware = (req, res, next) => {
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.status(401).json({message:'unauthorized'});
+    }
+    try{
+        const decoded = jwt.verify(token, jwtSecret)
+        req.userId = decoded.userId
+        next();
+    }catch(error){
+        res.status(401).json({message:'unauthorized'})
+    }
+}
+
+
+
 /*
 Get
 Admin - Login Page
@@ -29,17 +53,44 @@ Post
 Admin - Check Login
 */
 
-router.post('/admin', async(req, res)=>{
+router.post('/admin',async(req, res)=>{
     try {
 
         const{ username, password } = req.body;
-        console.log(req.body);
-        res.redirect('/admin')
+        
+        const user = await User.findOne({username});
+
+        if(!user){
+            return res.status(401).json({message:'Invalid credentials'})
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid){
+            return res.status(401).json({message:'Invalid credentials'})
+        }
+
+        const token = jwt.sign({userId:user._id}, jwtSecret )
+        res.cookie('token', token, {httpOnly:true});
+
+        res.redirect('/dashboard')
 
     } catch (error) {
         console.log(error)
     }
 });
+
+
+/*
+Post
+Admin - Check Login
+*/
+
+router.get('/dashboard', authMiddleware, async(req, res)=>{
+
+    res.render('admin/dashboard');
+
+    });
 
 /*
 Post
